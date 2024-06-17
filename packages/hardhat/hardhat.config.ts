27c1e9b28c50299d61fb4c41e0fd452573dcf204
Hyeon-hak <hyeonhak.kim.dev@gmail.com>
2024-06-17 13:35:40 +0900

    🛠️  Chore : vercel configuration

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -10,16 +10,12 @@ import "@nomicfoundation/hardhat-verify";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
 
-// If not set, it uses ours Alchemy's default API key.
-// You can get your own at https://dashboard.alchemyapi.io
-const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
-// If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey =
-  process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-// If not set, it uses ours Etherscan default API key.
-const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
+const providerApiKey = process.env.ALCHEMY_API_KEY!;
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY!;
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY!;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia",
   solidity: {
     version: "0.8.4",
     settings: {
@@ -30,7 +26,6 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  defaultNetwork: "localhost",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
