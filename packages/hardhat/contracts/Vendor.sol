40794d3dc1ed2f976832be11715b7e70b395a223
Hyeon-hak <hyeonhak.kim.dev@gmail.com>
2024-06-24 16:59:53 +0900

    ✨ Feat : contracts

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,50 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+interface IVendor {
+	function buyTokens() external payable;
 
-  YourToken public yourToken;
+	function withdraw() external;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+	function sellTokens(uint256 _amount) external;
+}
 
-  // ToDo: create a payable buyTokens() function:
+contract Vendor is IVendor, Ownable {
+	event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	event SellTokens(
+		address seller,
+		uint256 amountOfTokens,
+		uint256 amountOfETH
+	);
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+	YourToken public yourToken;
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+	constructor(address tokenAddress) {
+		yourToken = YourToken(tokenAddress);
+	}
+
+	function buyTokens() external payable override {
+		uint256 amountOfTokens = msg.value * tokensPerEth();
+		yourToken.transfer(msg.sender, amountOfTokens);
+		emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+	}
+
+	function withdraw() external override {
+		require(msg.sender == owner(), "Only the owner can withdraw");
+		payable(owner()).transfer(address(this).balance);
+	}
+
+	function sellTokens(uint256 _amount) external override {
+		yourToken.transferFrom(msg.sender, address(this), _amount);
+		uint256 amountOfTokens = _amount / tokensPerEth();
+		payable(msg.sender).transfer(amountOfTokens);
+		emit SellTokens(msg.sender, _amount, amountOfTokens);
+	}
+
+	function tokensPerEth() public pure returns (uint256) {
+		return 100;
+	}
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.4; //Do not change the solidity version as it negativly impa
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
-
 contract YourToken is ERC20 {
-  constructor() ERC20("Gold", "GLD") {
-    //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
-  }
+	uint256 private ONE_ETHER_IN_WEI = 10 ** decimals();
+
+	constructor() ERC20("Gold", "GLD") {
+		_mint(msg.sender, 1000 * ONE_ETHER_IN_WEI);
+	}
 }
